f2fa9d049cf0d57bab13c192d46b037bd7ce1111
thekururu <thekuruky@gmail.com>
2025-08-08 15:48:42 -0600

    Delete nuevo proyecto directory

--- nuevo proyecto/script.js
+++
@@ -1,7 +0,0 @@
-function changeMessage() {
-    const name = prompt("¿Cómo te llamas, amor?");
-    if (name) {
-        document.getElementById("nombre").innerText = name;
-        alert("Tu nombre es tan hermoso como tú, " + name + "!");
-    }
-}
